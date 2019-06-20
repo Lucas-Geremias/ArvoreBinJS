4c594bb99b0ac369f511253d5b1275a0e3f74068
Lucas Santos <geremiaslucas2012@gmail.com>
2019-06-20 19:00:23 -0300

    arvore

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//Declara uma variável local no escopo do bloco atual, opcionalmente iniciando-a com um valor.
+//let permite que você declare variáveis limitando seu escopo no bloco, instrução, ou em uma 
+//expressão na qual ela é usada. Isso é inverso da keyword var, que define uma variável globalmente 
+//ou no escopo inteiro de uma função, independentemente do escopo de bloco.
 let tree = new BinaryTree();
 $().ready(()=>{
     $('#btnInserir').click(inserir);
@@ -11,7 +15,12 @@ $().ready(()=>{
     $('#btnAltura').click(mostrarAltura);
     $('#btnTamanho').click(mostrarTamanho);
 });
+//aliado às facilidades da tipagem dinâmica, o que torna a definição de métodos simples e prática. 
+//Para criar funções, utilizamos a palavra reservada function.
+//As funções podem receber parâmetros e retornar valores, mas o tipo de retorno e o tipo dos 
+//parâmetros não precisa ser previamente definido.
 function mostrarItem(item){
+    //Variáveis declaradas com let são "içadas" para começo do bloco em que elas são definidas (isso é conhecido também pelo termo, hoisting).
     let el = $('#saida');
     el.empty();
     el.append(`<span class=gitdsfsdfdsfasdf"ui label">${item}</span>`);
@@ -19,7 +28,9 @@ function mostrarItem(item){
 function incluir(item){
     $('#saida').append(`<span class="ui label">${item}</span>`);
 }
-
+//Assim como a maioria das linguagens de alto nível, JavaScript possui estruturas condicionais e
+// de repetição para controle de fluxo
+//O bloco else pode ser omitido, caso apenas uma condição precise ser avaliada, dependendo do seu valor, uma mensagem é exibida na tela.
 function inserir(){
     let num = parseInt(prompt("informe o numero a inserir na arvore:"));
     if(tree.search(num))
